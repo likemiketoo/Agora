c8f1204a1f071f7d8b88b8fa6c023ab32410fb84
Michael Emefele <likemiketoo@hotmail.com>
2018-03-12 00:29:56 -0400

    Added fonts, Javascript,  changed naivigation bar
    Added font files and changed inital navigation bar to left navigation, added Javascript to make it work

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){												
+   //Navigation Menu Slider
+    $('#nav-expander').on('click',function(e)
+    {
+        e.preventDefault();
+        $('body').toggleClass('nav-expanded');
+    });
+    
+    $('#nav-close').on('click',function(e)
+    {
+        e.preventDefault();
+        $('body').removeClass('nav-expanded');
+    });
+    
+    $('.container-fluid').on('click',function(e)
+    {
+        e.preventDefault();
+        $('body').removeClass('nav-expanded');
+    });
+    
+    
+    
+    
+    var $root = $('html, body');
+    
+    $('a').click(function()
+    {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 900);
+        return false;
+    });
+    
+
+ });
