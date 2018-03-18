8fc80c16e668731d5b3d04fb62954d5aefcf6be1
Michael Emefele <likemiketoo@hotmail.com>
2018-03-18 02:32:05 -0400

    Added Learn More content and trimmed website height

--- js/app.js
+++ js/app.js
@@ -32,9 +32,25 @@ $(document).ready(function(){
         return false;
     });
     
+    //removes splash on click
+    
     $('#servText').on('click',function(e)
     {
         $('#splashDiv').remove();
      });
+    
+    
+    
+    $('#learnMore').click(function()
+    {
+        
+        $(splashText).empty();
+        $("#splashText").css(
+        {
+            fontSize: 18
+        });
+        $(splashText).append('The focus of this project is to enable entrepreneurs to distribute and raise awareness of their services and collect payment in a convenient manner through the mobile application and website. Both will allow for improved advertising of both goods and services on the campus of Norfolk State University where many talented entrepreneurs and students provide services.');
+        
+    });
 
  });
