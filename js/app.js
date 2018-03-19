ad7d621c206725e43a60e7da7d6cde980fbb85c2
Michael Emefele <likemiketoo@hotmail.com>
2018-03-19 17:24:15 -0400

    Set framework for page dynamically responding to button presses, added replacing title

--- js/app.js
+++ js/app.js
@@ -32,25 +32,51 @@ $(document).ready(function(){
         return false;
     });
     
-    //removes splash on click
-    
-    $('#servText').on('click',function(e)
-    {
-        $('#splashDiv').remove();
-     });
-    
-    
-    
     $('#learnMore').click(function()
     {
-        
-        $(splashText).empty();
-        $("#splashText").css(
+        //slides up and away
+        $('#splashText').slideUp('normal', function()
         {
-            fontSize: 18
+            //removes text
+            $(splashText).empty();
+            
+            //Changes text size
+            $("#splashText").css(
+            {
+                fontSize: 18
+            });
+            
+            //Appends new text
+            $('#splashText').slideDown('slow', function()
+            {
+            $(splashText).append('The focus of this project is to enable entrepreneurs to distribute and raise awareness of their services and collect payment in a convenient manner through the mobile application and website. Both will allow for improved advertising of both goods and services on the campus of Norfolk State University where many talented entrepreneurs and students provide services.');
+            });
         });
-        $(splashText).append('The focus of this project is to enable entrepreneurs to distribute and raise awareness of their services and collect payment in a convenient manner through the mobile application and website. Both will allow for improved advertising of both goods and services on the campus of Norfolk State University where many talented entrepreneurs and students provide services.');
         
     });
+    
+    //removes splash on click
+    $('#servTextHair, #servTextTutor, #servTextNails, #servTextSalon, #servTextShoes, #servTextRepair, #servTextTailor, #servTextProducer, #servTextTrainer').on('click',function(e)
+    {
+        //slides up then hides div at normal speed
+        $('#splashDiv').slideUp('normal', function()
+        {
+            $('#splashDiv').remove();
+        });
+     });
+
+    //replace Label on click
+    $('#servTextHair').on('click',function(e)
+    {
+        //slides up then hides div at normal speed
+        $('#servLabelDiv').empty();
+        $('#servLabelDiv').css(
+            {
+                fontSize: 65
+            });
+        $('#servLabelDiv').append('Entrepreneurs');
+        $('#servLabelDiv').append('<div class="container" id="servSub">Entrepreneurs offering service</div>');
+     });
+    
 
  });
