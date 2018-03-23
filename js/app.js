bcdecb42be560761c2cc620e41318b82825e4d9e
Michael Emefele <likemiketoo@hotmail.com>
2018-03-23 01:13:07 -0400

    Converted to bootstrap 4 from 3

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@ $(document).ready(function(){
         e.preventDefault();
         $('body').removeClass('nav-expanded');
     });
-    
-    $('.container-fluid').on('click',function(e)
-    {
-        e.preventDefault();
-        $('body').removeClass('nav-expanded');
-    });
-    
+//    
+//    $('.container-fluid').on('click',function(e)
+//    {
+//        e.preventDefault();
+//        $('body').removeClass('nav-expanded');
+//    });
+//    
     
     
     
@@ -43,7 +43,7 @@ $(document).ready(function(){
             //Changes text size
             $("#splashText").css(
             {
-                fontSize: 18
+                fontSize: 15
             });
             
             //Appends new text
@@ -56,65 +56,65 @@ $(document).ready(function(){
     });
     
     //removes splash on click
-    $('#servTextHair, #servTextTutor, #servTextNails, #servTextSalon, #servTextShoes, #servTextRepair, #servTextTailor, #servTextProducer, #servTextTrainer').on('click',function(e)
-    {
-        //slides up then hides div at normal speed
-        $('#splashDiv').slideUp('normal', function()
-        {
-            $('#splashDiv').remove();
-        });
-		
-		//slides up then hides div at normal speed
-        $('#servLabelDiv').empty();
-		//changes css
-        $('#servLabelDiv').css(
-            {
-                
-            });
-		//
-        $('#servLabelDiv').append('Entrepreneurs');
-        $('#servLabelDiv').append('<div class="container" id="servSub">Entrepreneurs offering service</div>');
-     });
-
-    //replace Label on click
-    $('#servTextHair').on('click',function(e)
-    {
-    	$('#servDivInner').empty();
-		$('#servDivInner').append('<div class="row" id="servRow"> <div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx1"> </div><div class="container" id="barberEx1Text">Bob The Barber </div></a> </div><div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx2"> </div><div class="container" id="barberEx2Text">Big Bro\'s</div></a> </div><div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx3"> </div><div class="container" id="barberEx3Text">Super Kutz </div></a> </div></div><div class="row" id="servRow"> <div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx4"> </div><div class="container" id="barberEx4Text">John Cuttington </div></a> </div></div>');
-    });
-	
-	$('#barberEx1').on('click',function(e)
-    {
-		$('#servLabelDiv').empty();
-		$('#servLabelDiv').append('Bob The Barber');
-		
-		$('#servDivInner2').empty();
-	});
-	
-	$('#barberEx2').on('click',function(e)
-    {
-		$('#servLabelDiv').empty();
-		$('#servLabelDiv').append('Big Bro\'s');
-		
-		$('#servDivInner2').empty();
-	});
-	
-	
-	$('#barberEx3').on('click',function(e)
-    {
-		$('#servLabelDiv').empty();
-		$('#servLabelDiv').append('Super Kutz');
-		
-		$('#servDivInner2').empty();
-	});
-	
-	$('#barberEx4').on('click',function(e)
-    {
-		$('#servLabelDiv').empty();
-		$('#servLabelDiv').append('John Cuttington');
-		
-		$('#servDivInner2').empty();
-	});
+//    $('#servTextHair, #servTextTutor, #servTextNails, #servTextSalon, #servTextShoes, #servTextRepair, #servTextTailor, #servTextProducer, #servTextTrainer').on('click',function(e)
+//    {
+//        //slides up then hides div at normal speed
+//        $('#splashDiv').slideUp('normal', function()
+//        {
+//            $('#splashDiv').remove();
+//        });
+//		
+//		//slides up then hides div at normal speed
+//        $('#servLabelDiv').empty();
+//		//changes css
+//        $('#servLabelDiv').css(
+//            {
+//                
+//            });
+//		//
+//        $('#servLabelDiv').append('Entrepreneurs');
+//        $('#servLabelDiv').append('<div class="container" id="servSub">Entrepreneurs offering service</div>');
+//     });
+//
+//    //replace Label on click
+//    $('#servTextHair').on('click',function(e)
+//    {
+//    	$('#servDivInner').empty();
+//		$('#servDivInner').append('<div class="row" id="servRow"> <div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx1"> </div><div class="container" id="barberEx1Text">Bob The Barber </div></a> </div><div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx2"> </div><div class="container" id="barberEx2Text">Big Bro\'s</div></a> </div><div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx3"> </div><div class="container" id="barberEx3Text">Super Kutz </div></a> </div></div><div class="row" id="servRow"> <div class="col-xs-4" id="servCol"> <a> <div class="container" id="barberEx4"> </div><div class="container" id="barberEx4Text">John Cuttington </div></a> </div></div>');
+//    });
+//	
+//	$('#barberEx1').on('click',function(e)
+//    {
+//		$('#servLabelDiv').empty();
+//		$('#servLabelDiv').append('Bob The Barber');
+//		
+//		$('#servDivInner2').empty();
+//	});
+//	
+//	$('#barberEx2').on('click',function(e)
+//    {
+//		$('#servLabelDiv').empty();
+//		$('#servLabelDiv').append('Big Bro\'s');
+//		
+//		$('#servDivInner2').empty();
+//	});
+//	
+//	
+//	$('#barberEx3').on('click',function(e)
+//    {
+//		$('#servLabelDiv').empty();
+//		$('#servLabelDiv').append('Super Kutz');
+//		
+//		$('#servDivInner2').empty();
+//	});
+//	
+//	$('#barberEx4').on('click',function(e)
+//    {
+//		$('#servLabelDiv').empty();
+//		$('#servLabelDiv').append('John Cuttington');
+//		
+//		$('#servDivInner2').empty();
+//	});
 
  });
 
