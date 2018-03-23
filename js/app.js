e174b3a7331e45d0e147bcee3322f14ab62d4335
Michael Emefele <likemiketoo@hotmail.com>
2018-03-23 03:37:52 -0400

    Added framework for individual's pages, pay and contact buttons

--- js/app.js
+++ js/app.js
@@ -24,13 +24,13 @@ $(document).ready(function(){
     
     var $root = $('html, body');
     
-    $('a').click(function()
-    {
-        $root.animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 900);
-        return false;
-    });
+//    $('a').click(function()
+//    {
+//        $root.animate({
+//            scrollTop: $( $.attr(this, 'href') ).offset().top
+//        }, 900);
+//        return false;
+//    });
     
     $('#learnMore').click(function()
     {
